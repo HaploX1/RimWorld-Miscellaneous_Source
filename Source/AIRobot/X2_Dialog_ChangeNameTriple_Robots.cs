4e06664087f1637385ce6ff3e1c040cd6a218500
Markus K <Silverfalcon@gmx.net>
2018-02-05 22:16:12 +0100

    Added rename option to robots menu

---
+++ Source/AIRobot/X2_Dialog_ChangeNameTriple_Robots.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Verse.AI;
+
+namespace AIRobot
+{
+    // Based of Verse.Dialog_ChangeNameTriple
+    public class X2_Dialog_ChangeNameTriple_Robots : Window
+    {
+
+        private Pawn pawn;
+        private string curName;
+        private const int MaxNameLength = 16;
+
+        private NameTriple CurPawnName
+        {
+            get
+            {
+                NameTriple nameTriple = this.pawn.Name as NameTriple;
+                if (nameTriple != null)
+                {
+                    return new NameTriple(nameTriple.First, this.curName, nameTriple.Last);
+                }
+                throw new InvalidOperationException();
+            }
+        }
+
+        public override Vector2 InitialSize
+        {
+            get
+            {
+                return new Vector2(500f, 175f);
+            }
+        }
+
+        public X2_Dialog_ChangeNameTriple_Robots(Pawn pawn)
+        {
+            this.pawn = pawn;
+            this.curName = ((NameTriple)pawn.Name).Nick;
+            base.forcePause = true;
+            base.absorbInputAroundWindow = true;
+            base.closeOnClickedOutside = true;
+        }
+
+        public override void DoWindowContents(Rect inRect)
+        {
+            bool flag = false;
+            if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return)
+            {
+                flag = true;
+                Event.current.Use();
+            }
+            Text.Font = GameFont.Medium;
+            Widgets.Label(new Rect(15f, 15f, 500f, 50f), this.CurPawnName.ToString().Replace(" '' ", " "));
+            Text.Font = GameFont.Small;
+            string text = Widgets.TextField(new Rect(15f, 50f, inRect.width / 2f - 20f, 35f), this.curName);
+            if (text.Length < 16)
+            {
+                this.curName = text;
+            }
+            if (!Widgets.ButtonText(new Rect(inRect.width / 2f + 20f, inRect.height - 35f, inRect.width / 2f - 20f, 35f), "OK".Translate(), true, false, true) && !flag)
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(this.curName))
+            {
+                this.curName = ((NameTriple)this.pawn.Name).First;
+            }
+            this.pawn.Name = this.CurPawnName;
+            Find.WindowStack.TryRemove(this, true);
+            //Messages.Message("PawnGainsName".Translate(this.curName), this.pawn, MessageTypeDefOf.PositiveEvent);
+
+            Messages.Message("AIRobot_RobotIsRenamed".Translate(this.curName), this.pawn, MessageTypeDefOf.PositiveEvent);
+        }
+
+    }
+}
---
+++ Source/AIRobot/X2_PawnColumnWorker_Rename.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Verse.AI;
+
+namespace AIRobot
+{
+    [StaticConstructorOnStartup]
+    public class X2_PawnColumnWorker_Rename : PawnColumnWorker_Label
+    {
+        public static readonly Texture2D texRename = ContentFinder<Texture2D>.Get("UI/Buttons/Rename", true);
+        public static string nameFirst = "AIRobot_Basename_first";
+        //public static string nameNick = "AIRobot_Basename_nick";
+        //public static string nameLast = "AIRobot_Basename_last";
+
+        public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
+        {
+            if (Widgets.ButtonImage(rect, texRename))
+            {
+
+                if (pawn.Name == null)
+                {
+                    NameTriple name = new NameTriple(nameFirst.Translate(), pawn.Label, pawn.Label);
+                    pawn.Name = name;
+                }
+
+                //Log.Error(pawn == null ? "pawn==null" : pawn.Label);
+                //Log.Error(pawn.Name == null ? "pawn.Name==null" : pawn.Name as NameTriple == null ? "pawn.Name as NameTriple==null" : (pawn.Name as NameTriple).Nick);
+
+                //Find.WindowStack.Add( new Dialog_ChangeNameTriple(pawn) );
+                Find.WindowStack.Add(new X2_Dialog_ChangeNameTriple_Robots(pawn));
+            }
+            if (Mouse.IsOver(rect))
+            {
+                GUI.DrawTexture(rect, TexUI.HighlightTex);
+            }
+            TipSignal tooltip = pawn.Label;
+            tooltip.text = "AIRobot_RenameRobot".Translate();
+            TooltipHandler.TipRegion(rect, tooltip);
+        }
+
+        public override int GetMinWidth(PawnTable table)
+        {
+            return 30;
+        }
+
+        public override int GetOptimalWidth(PawnTable table)
+        {
+            return Mathf.Clamp(30, this.GetMinWidth(table), this.GetMaxWidth(table));
+        }
+
+        public override int GetMaxWidth(PawnTable table)
+        {
+            return Mathf.Min(base.GetMaxWidth(table), this.GetMinWidth(table));
+        }
+
+    }
+}
